9016005b758e97693ff81469a08ae41ee9bae745
LJRollerII <ljcoolkid@optonline.net>
2022-10-19 19:39:55 -0400

    Broke address into individual columns.

--- scripts/nashville_housing.sql
+++ scripts/nashville_housing.sql
@@ -82,4 +82,93 @@ JOIN nashville_housing AS b
 WHERE a.property_address IS NULL
 
 --===============================================================================================================--
---Breaking out address into individual columns (Adress, City, State)
+--Breaking out address into individual columns (Address, City, State)
+--We will do this with the property address and owner's address columns
+
+SELECT property_address
+FROM nashville_housing
+
+--We'll need to use a substring
+--Substrings require 3 arguments
+--For the substring 1 = position 1
+--Use -1 to get rid of the comma when you run the query.
+--For the second substring CHARINDEX(',', property_address) + 1 will be the position
+SELECT
+SUBSTRING(property_address, 1, CHARINDEX(',', PropertyAddress) -1) AS address,
+SUBSTRING(property_address, CHARINDEX(',', property_address) + 1 , LEN(property_address)) AS address
+FROM nashville_housing
+
+--We'll need to add two new cloumns to seperate City & State
+--In python you can seperate two values in one column without making two new columns.
+
+ALTER TABLE nashville_housing
+ADD popertysplit_address Nvarchar(255);
+
+UPDATE nashville_housing
+SET popertysplit_address = SUBSTRING(property_address, 1, CHARINDEX(',', PropertyAddress) -1)
+
+
+ALTER TABLE nashville_housing
+ADD popertysplit_city Nvarchar(255);
+
+UPDATE nashville_housing
+SET popertysplit_city = SUBSTRING(property_address, CHARINDEX(',', property_address) + 1 , LEN(property_address))
+
+--Let's use select all once we've altered and updated table to see if the new columns/updates are present.
+SELECT *
+FROM nashville_housing
+
+
+
+--Now let's work on the owner's address
+SELECT owner_address
+FROM nashville_housing
+
+--In this query we'll use PARSENAME
+--PARSENAME is only useful with periods, so we'll have to replace our commas with periods.
+--We use 3,2,1 because that will give us the Address, City, & State in the order we want.
+--Using 1,2,3 would make our order(Address, City, & State) backwards.
+SELECT
+PARSENAME(REPLACE(owner_address, ',', '.') , 3),
+PARSENAME(REPLACE(owner_address, ',', '.') , 2),
+PARSENAME(REPLACE(owner_address, ',', '.') , 1)
+FROM nashville_housing
+
+ALTER TABLE nashville_housing
+ADD ownersplit_address Nvarchar(255);
+
+UPDATE nashville_housing
+SET ownersplit_address = PARSENAME(REPLACE(owner_address, ',', '.') , 3)
+
+
+ALTER TABLE nashville_housing
+ADD ownersplit_city Nvarchar(255);
+
+UPDATE nashville_housing
+SET ownersplit_city = PARSENAME(REPLACE(owner_address, ',', '.') , 2)
+
+ALTER TABLE nashville_housing
+ADD ownersplit_state Nvarchar(255);
+
+UPDATE nashville_housing
+SET ownersplit_state = PARSENAME(REPLACE(owner_address, ',', '.') , 1)
+
+--Let's use select all once we've altered and updated table to see if the new columns/updates are present.
+SELECT *
+FROM nashville_housing
+
+--===============================================================================================================--
+
+-- Change Y and N to Yes and No in "Sold as Vacant" column
+
+
+--===============================================================================================================--
+-- Remove Duplicates
+
+
+--===============================================================================================================--
+-- Delete Unused Columns
+
+
+--===============================================================================================================--
+--- Importing Data using OPENROWSET and BULK INSERT
